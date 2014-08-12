34f2cd8ce65b678d1aa3bcf4506acceef698bf4f
Gao Han <gaohan@amazon.com>
2014-08-11 17:47:47 -0700

    deleted unnecessary cookbooks

--- opsworks_commons/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        "opsworks_commons"
-description "Common recipes and libraries"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "1.0.0"
-
-# this cookbook should not have dependencies
