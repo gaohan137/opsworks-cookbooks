77968ee3eff1ecf353c12c6c0d790154fdca134d
Gao Han <gaohan@amazon.com>
2014-08-17 22:23:54 -0700

    Add rails_custom_layer2

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -2,3 +2,8 @@ default[:custom_layers][:rails_layer_custom][:opsworks_bundler][:manage_package]
 default[:custom_layers][:rails_layer_custom][:apps] = ['rails_custom1']
 default[:custom_layers][:rails_layer_custom][:layer_type] = 'rails-app'
 default[:custom_layers][:rails_layer_custom][:rails_stack][:name] = 'apache_passenger'
+
+default[:custom_layers][:rails_layer_custom2][:opsworks_bundler][:manage_package] = true
+default[:custom_layers][:rails_layer_custom2][:apps] = ['rails_custom2']
+default[:custom_layers][:rails_layer_custom2][:layer_type] = 'rails-app'
+default[:custom_layers][:rails_layer_custom2][:rails_stack][:name] = 'apache_passenger'
