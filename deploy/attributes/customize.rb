8fffc4dddbe5a960cf026b893c803d1e4741683b
Gao Han <gaohan@amazon.com>
2014-08-18 18:35:05 -0700

    added unicorn

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -7,3 +7,8 @@ default[:custom_layers][:rails_layer_custom2][:opsworks_bundler][:manage_package
 default[:custom_layers][:rails_layer_custom2][:apps] = ['rails_custom2']
 default[:custom_layers][:rails_layer_custom2][:layer_type] = 'rails-app'
 default[:custom_layers][:rails_layer_custom2][:rails_stack][:name] = 'apache_passenger'
+
+default[:custom_layers][:unicorn_layer_custom][:opsworks_bundler][:manage_package] = true
+default[:custom_layers][:unicorn_layer_custom][:apps] = ['unicorn_custom1']
+default[:custom_layers][:unicorn_layer_custom][:layer_type] = 'rails-app'
+default[:custom_layers][:unicorn_layer_custom][:rails_stack][:name] = 'nginx_unicorn'
